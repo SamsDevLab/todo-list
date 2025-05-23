9dd186ef3ad3836610ac71d8965039408c85a628
Sam Hadfield <sthtech@protonmail.com>
2025-05-23 14:39:59 -0500

    Remove comments

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,26 +1,3 @@
-/* 
-- Create Todo object through a function
-- Object includes:
-    • Title
-    • Due Date
-    • Priority
-    • Notes
-    • Choose the Project
-    • Checklist of Subtasks
-    • Completed? (This will be a toggle or a checkbox in DOM)
-*/
-
-/* 
-Start fresh here tomorrow
- • Work on pseudocode
- • This module needs to do everything 'Todo'-related
- • It needs to:
-    - Create the todo
-    - Choose a project
-    - Set todo as complete
-    - Change todo priority
-*/
-
 export const todo = function () {
   const createTodo = function (
     title,
