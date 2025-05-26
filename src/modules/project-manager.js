1fbfff817c3cc3b731062115535ad293ebc4d906
Sam Hadfield <sthtech@protonmail.com>
2025-05-26 08:58:16 -0500

    Create project manager to store all projects
    Create project manager to store all projects in a project array. This
    will help with DOM interaction.

---
+++ src/modules/project-manager.js
@@ -0,0 +1,12 @@
+/*
+• Have a project's array to manage all of the projects
+  - This will allow them to interact with the DOM
+• Place todo object in project's array?
+*/
+
+export const projManager = function (project) {
+  const projArr = [];
+  projArr.push(project);
+
+  console.log(projArr);
+};
