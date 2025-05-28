10b55a8bf7bfe77bb607dc4a9a9615d02a328636
Sam Hadfield <sthtech@protonmail.com>
2025-05-28 17:39:39 -0500

    Import dom from dom.js to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./styles.css";
 import { createTodo } from "./modules/todo.js";
 import { createProject } from "./modules/project.js";
 import { projManager } from "./modules/project-manager.js";
+import { dom } from "./modules/dom.js";
 
 // Exports:
 export { createTodo }; // to project-manager.js
