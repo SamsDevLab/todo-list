5277c4a86a597093a5d0229f38a1f8ba217c65bf
Sam Hadfield <sthtech@protonmail.com>
2025-05-29 12:34:23 -0500

    Add click listeners to 'data-action' buttons
    Add click event listeners to both 'data-action' buttons. One button
    allows user to add a new Todo - the other allows user to add a new Project.
    Depending on which button the user clicks, a modal will open allowing
    user to add a new Todo or a new Project

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,3 +1,44 @@
 import { projManager } from "/src/index.js";
 
-export const dom = () => {};
+export const dom = () => {
+  // Grab Main Container
+  const mainContainer = document.querySelector("#main-container");
+
+  // Grab New Todo and New Project Button via data-attributes
+  const sectionHeaderBtns = document.querySelectorAll("[data-action]");
+  sectionHeaderBtns.forEach((button) =>
+    button.addEventListener("click", (event) => {
+      if (event.target.dataset.action === "add-todo") {
+        openTodo();
+      } else if (event.target.dataset.action === "add-project") {
+        openProject();
+      }
+    })
+  );
+
+  /////// Modals /////////
+
+  // Open Todo Modal
+  const openTodo = () => {
+    const todoModal = document.querySelector('[data-modal="open-todo"]');
+    todoModal.showModal();
+  };
+
+  // Open Project Modal
+  const openProject = () => {
+    const projModal = document.querySelector('[data-modal="open-project"]');
+    projModal.showModal();
+  };
+
+  ////// SideBar /////////
+  // Create Project Buttons in Sidebar
+
+  // Create Todo List in Sidebar (optional)
+
+  /////// Body ///////
+  // Pass in Project Name to h2
+
+  // Create Todo Divs
+};
+
+dom();
