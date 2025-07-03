db2b39a1cb70fe1ba378382948e5ae5688543f35
Sam Hadfield <sthtech@protonmail.com>
2025-07-03 12:30:47 -0500

    Store projs and todos in localStorage
    Store proj and todo objs in localStorage via saveToLocalStorage() fn
    in storage.js. This function will differentiate between the two objs,
    then store them in localStorage according to whether they are projects
    or todos

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,6 +1,12 @@
-export const localStorage = function () {
-  const saveToLocalStorage = () => {
-    console.log("Dankman");
+export const editlocalStorage = function () {
+  const saveToLocalStorage = (obj) => {
+    const jsonString = JSON.stringify(obj);
+
+    if (jsonString.includes("id") && jsonString.includes("projId")) {
+      localStorage.setItem(`todo-${obj.title}`, jsonString);
+    } else {
+      localStorage.setItem(`proj-${obj.name}`, jsonString);
+    }
   };
   const getFromLocalStorage = () => {
     console.log("even danker");
