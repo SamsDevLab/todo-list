fce1b3a8297024888a3a82504cde55398b936baa
Sam Hadfield <sthtech@protonmail.com>
2025-07-03 13:57:16 -0500

    Change naming convention in saveToLocalStorage to utilize ids rather than names

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -3,9 +3,9 @@ export const editlocalStorage = function () {
     const jsonString = JSON.stringify(obj);
 
     if (jsonString.includes("id") && jsonString.includes("projId")) {
-      localStorage.setItem(`todo-${obj.title}`, jsonString);
+      localStorage.setItem(`todo-${obj.id}`, jsonString);
     } else {
-      localStorage.setItem(`proj-${obj.name}`, jsonString);
+      localStorage.setItem(`proj-${obj.id}`, jsonString);
     }
   };
   const getFromLocalStorage = () => {
