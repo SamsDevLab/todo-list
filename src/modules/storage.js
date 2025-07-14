aad21957d11cbdb9212cafdb9383f60a02cf4a82
Sam Hadfield <sthtech@protonmail.com>
2025-07-14 16:48:22 -0500

    Refactor fns to create proj/todo arrs in storage
    Refactor "saveToLocalStorage()". Previously, this would add a project or
    todo to localStorage in no particular order. Now, it routes the object
    based on whether it's a todo or a project, then checks for a todo/proj
    arr within localStorage to place the object. If it can't find an array,
    it makes an array, then stores the object. This keeps projs and todos
    organized upon reloading the page
    
    Refactor getLocalStorageObjs() and editTodoInLocalStorage() to reflect
    above changes.

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,34 +1,88 @@
 export const editlocalStorage = function () {
-  // Save to Storage
-  const saveToLocalStorage = (obj) => {
-    const jsonString = JSON.stringify(obj);
+  const addTodoToLocalStorage = (todo) => {
+    const todoArrStr = localStorage.getItem("todoArr");
 
-    if (jsonString.includes("id") && jsonString.includes("projId")) {
-      localStorage.setItem(`todo-${obj.id}`, jsonString);
+    if (todoArrStr !== null) {
+      const parsedTodoArr = JSON.parse(todoArrStr);
+      parsedTodoArr.push(todo);
+      const newTodoArrStr = JSON.stringify(parsedTodoArr);
+      localStorage.setItem("todoArr", newTodoArrStr);
     } else {
-      localStorage.setItem(`proj-${obj.id}`, jsonString); //
+      const todoArr = [];
+      todoArr.push(todo);
+      const newTodoArrStr = JSON.stringify(todoArr);
+      localStorage.setItem("todoArr", newTodoArrStr);
     }
   };
 
-  const getLocalStorageObjs = () => {
-    const storageItems = { ...localStorage };
+  const addProjToLocalStorage = (proj) => {
+    const projArrStr = localStorage.getItem("projArr");
 
-    return storageItems;
+    if (projArrStr !== null) {
+      const parsedProjArr = JSON.parse(projArrStr);
+      parsedProjArr.push(proj);
+      const newProjArrStr = JSON.stringify(parsedProjArr);
+      localStorage.setItem("projArr", newProjArrStr);
+    } else {
+      const localStorageProjArr = [];
+      localStorageProjArr.push(proj);
+      const newProjArrStr = JSON.stringify(localStorageProjArr);
+      localStorage.setItem("projArr", newProjArrStr);
+    }
   };
 
-  // Edit Todo
+  const saveToLocalStorage = (obj) => {
+    if ("id" in obj === true && "projId" in obj === true) {
+      addTodoToLocalStorage(obj);
+    } else {
+      addProjToLocalStorage(obj);
+    }
+  };
+
+  // Projects:
+  const getLocalStorageProjs = () => {
+    const projArrStr = localStorage.getItem("projArr");
+
+    if (projArrStr !== null) {
+      const parsedProjArr = JSON.parse(projArrStr);
+      return parsedProjArr;
+    } else return;
+  };
+
+  // Todos:
+  const getLocalStorageTodos = () => {
+    const todoArrStr = localStorage.getItem("todoArr");
+
+    if (todoArrStr !== null) {
+      const parsedTodoArr = JSON.parse(todoArrStr);
+      return parsedTodoArr;
+    } else return;
+  };
+
+  const updateStoredTodoValues = (targetTodo, editedTodo) => {
+    targetTodo.title = editedTodo.title;
+    targetTodo.dueDate = editedTodo.dueDate;
+    targetTodo.priority = editedTodo.priority;
+    targetTodo.details = editedTodo.details;
+    targetTodo.project = editedTodo.project;
+
+    return targetTodo;
+  };
+
+  // Second Draft:
   const editTodoInLocalStorage = (editedTodo) => {
-    const storageItems = { ...localStorage };
-    const todoId = editedTodo.id;
-    let targetTodo = "";
-    for (const [key, value] of Object.entries(storageItems)) {
-      if (key.endsWith(todoId)) {
-        targetTodo = value;
-      }
-    }
-    const parsedTodo = JSON.parse(targetTodo);
+    const localTodoArrStr = localStorage.getItem("todoArr");
+    const parsedLocalTodoArr = JSON.parse(localTodoArrStr);
 
-    return parsedTodo;
+    const targetTodo = parsedLocalTodoArr.find(
+      (todo) => todo.id === editedTodo.id
+    );
+
+    updateStoredTodoValues(targetTodo, editedTodo);
+
+    const stringifiedTodoArr = JSON.stringify(parsedLocalTodoArr);
+
+    localStorage.setItem("todoArr", stringifiedTodoArr);
   };
 
   const removeObjFromLocalStorage = (key) => {
@@ -37,6 +91,7 @@ export const editlocalStorage = function () {
 
   // Remove From Local Storage
   const removeFromLocalStorage = (objToRemove) => {
+    console.log(objToRemove);
     const storageItems = { ...localStorage };
     for (const [key, value] of Object.entries(storageItems)) {
       if (
@@ -51,9 +106,11 @@ export const editlocalStorage = function () {
   const clearLocalStorage = () => {
     console.log("RZA");
   };
+
   return {
     saveToLocalStorage,
-    getLocalStorageObjs,
+    getLocalStorageProjs,
+    getLocalStorageTodos,
     editTodoInLocalStorage,
     removeFromLocalStorage,
     clearLocalStorage,
